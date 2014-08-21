0a93c1cb85717699d330b9113d344ca0cdb407d2
The Mini John <admin@phoenix-prime.gr>
2014-08-21 21:16:25 +0300

    Moving our Views

--- assets/js/entities/contact.js
+++ assets/js/entities/contact.js
@@ -8,4 +8,36 @@ ContactManager.module('Entities', function(Entities, ContactManager, Backbone, M
 		model: Entities.Contact,
 		comparator: 'firstName'
 	})
+
+
+
+	var contacts;
+
+	var initializeContacts = function() {
+		contacts = new Entities.ContactCollection([
+			{ id: 1, firstName: 'Alice', lastName: 'Artent',
+				phoneNumber: '555-0184'},
+			{ id: 2, firstName: 'Bob', lastName: 'Brigham',
+				phoneNumber: '555-0163'},
+			{ id: 3, firstName: 'Charlie', lastName: 'Campbell',
+				phoneNumber: '555-0129'}
+		]);
+	};
+
+	var API = {
+		getContactEntities: function(){
+			if(contacts === undefined){
+				initializeContacts();
+			}
+			return contacts;
+		}
+	};
+
+
+
+	ContactManager.reqres.setHandler("contact:entities", function(){
+		return API.getContactEntities();
+	});
+
+
 });
